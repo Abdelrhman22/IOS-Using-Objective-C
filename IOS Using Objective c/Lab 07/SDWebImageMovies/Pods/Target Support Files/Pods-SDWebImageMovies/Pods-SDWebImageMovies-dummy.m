64945681bb4a248664938f2562086abda45a9d78
Arafa <abdoarafa72@yahoo.com>
2019-04-27 10:06:05 -0700

    SDWebImage

---
+++ IOS Using Objective c/Lab 07/SDWebImageMovies/Pods/Target Support Files/Pods-SDWebImageMovies/Pods-SDWebImageMovies-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SDWebImageMovies : NSObject
+@end
+@implementation PodsDummy_Pods_SDWebImageMovies
+@end
---
+++ IOS Using Objective c/Lab 07/SDWebImageMovies/SDWebImageMovies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDWebImageMovies
+//
+//  Created by Esraa Hassan on 4/27/19.
+//  Copyright © 2019 Esraa And Passant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
