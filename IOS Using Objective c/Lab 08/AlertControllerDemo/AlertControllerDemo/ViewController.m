edb21111e1dba6445b14637f438cf60fa5af8b73
Abd elrhman Arafa Hassanien <abdoarafa72@yahoo.com>
2019-05-02 13:04:32 +0200

    Lab 8

--- IOS Using Objective c/Lab 08/AlertControllerDemo/AlertControllerDemo/ViewController.m
+++ IOS Using Objective c/Lab 08/AlertControllerDemo/AlertControllerDemo/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 - (IBAction)showAlert:(UIButton *)sender;
+- (IBAction)takePhoto:(UIButton *)sender;
+- (IBAction)choosePhoto:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UIImageView *image;
 
 @end
 
@@ -47,5 +50,50 @@
     [alert addAction:cancel];
     
     [self presentViewController:alert animated:YES completion:nil];
+}
+
+- (IBAction)takePhoto:(UIButton *)sender
+{
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        UIImagePickerController *picker = [UIImagePickerController new];
+        picker.allowsEditing=YES;
+        picker.delegate=self;
+        picker.sourceType=UIImagePickerControllerSourceTypeCamera;
+        
+        [self presentViewController:picker animated:YES completion:nil];
+    }
+    else
+    {
+        UIAlertController * alert = [UIAlertController
+                                     alertControllerWithTitle:@"Alert"
+                                     message:@"No Camera Found"
+                                     preferredStyle:UIAlertControllerStyleActionSheet];
+        UIAlertAction* ok = [UIAlertAction
+                             actionWithTitle:@"ok"
+                             style:UIAlertActionStyleDefault
+                             handler:^(UIAlertAction * action) {
+                                 printf("OK \n");
+                             }];
+      
+        [alert addAction:ok];
+        
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+    
+}
+- (IBAction)choosePhoto:(UIButton *)sender
+{
+    UIImagePickerController *picker = [UIImagePickerController new];
+    picker.allowsEditing=YES;
+    picker.delegate=self;
+    picker.sourceType=UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:picker animated:YES completion:nil];
+}
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info
+{
+    _image.image=info[UIImagePickerControllerEditedImage];
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 @end
